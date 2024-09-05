dcfbf87668c645debefa801de830a43b4e1d03ca
asher gray <asher@themakerspace.co.za>
2024-09-05 10:58:25 +0200

    changed to use enter to send high signal

--- test.py
+++ test.py
@@ -1,39 +1,32 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED
+from signal import pause
 import time
-import keyboard
-
-# Set the GPIO mode to BCM
-GPIO.setmode(GPIO.BCM)
 
 # Choose a GPIO pin (e.g., GPIO 18)
 output_pin = 18
 
-# Set up the pin as an output
-GPIO.setup(output_pin, GPIO.OUT)
+# Create an LED object (which we'll use to control our output)
+led = LED(output_pin)
 
 # Duration of the high signal in seconds
 signal_duration = 0.1
 
 def send_high_signal():
-    GPIO.output(output_pin, GPIO.HIGH)
+    led.on()
     time.sleep(signal_duration)
-    GPIO.output(output_pin, GPIO.LOW)
+    led.off()
     print("Sent a high signal")
 
+print("Press Enter to send a high signal. Press Ctrl+C to quit.")
+
 try:
-    print("Press 'w' to send a high signal. Press 'q' to quit.")
     while True:
-        if keyboard.is_pressed('w'):
-            send_high_signal()
-            # Wait a bit to avoid multiple signals from one press
-            time.sleep(0.2)
-        elif keyboard.is_pressed('q'):
-            print("Quitting...")
-            break
+        input("Press Enter to send signal...")
+        send_high_signal()
 
 except KeyboardInterrupt:
-    print("Program stopped by user")
+    print("\nProgram stopped by user")
 
 finally:
-    # Clean up the GPIO settings
-    GPIO.cleanup()
+    # The gpiozero library automatically cleans up on exit
+    pass
